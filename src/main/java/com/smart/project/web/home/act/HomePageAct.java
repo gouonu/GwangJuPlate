ea5dfdc8b4a09a69f6d1d19a640d1220d5d97dc6
이성은 <seongeun47@naver.com>
2022-11-18 13:25:25 +0900

    검색 시 템플릿 append 완료

--- src/main/java/com/smart/project/web/home/act/HomePageAct.java
+++ src/main/java/com/smart/project/web/home/act/HomePageAct.java
@@ -1,14 +1,16 @@
 package com.smart.project.web.home.act;
 
 import com.smart.project.proc.Test;
+import com.smart.project.web.home.vo.MainVO;
 import com.smart.project.web.home.vo.ResVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,11 +24,24 @@ public class HomePageAct {
 
         List<ResVO> r = test.selectRes(input);
         model.addAttribute("res", r);
-
-
-
-
+        model.addAttribute("input", input);
         return "search2";
     }
+
+    @PostMapping ("/searchInput")
+    @ResponseBody
+    public Map getSearch(@RequestBody Map map) {
+        Map<String, Object> data = new HashMap<>();
+        String param = String.valueOf(map.get("query")); // 이부분 잘모름
+        List<ResVO> r = test.selectRes(param);
+        data.put("r",r);
+        return data;
+    }
+
+
+
+
+
+
 
 }
--- src/main/resources/static/resources/js/search/search.js
+++ src/main/resources/static/resources/js/search/search.js
@@ -1,10 +1,37 @@
 "use strict";
 
 
+import listTemplate from "@/main/listCard.html";
+import searchTemplate from "@/search/seach2-1.html";
+
 $(()=>{
     new Search();
 })
 
 export class Search {
+
+    constructor() {
+        console.log("Search");
+        this.searchEvent();
+    }
+
+    searchEvent() {
+        console.log("검색 이벤트");
+        let query = $('#query').text();
+        console.log(query)
+        let searchTemplate = require('@/search/seach2-1.html');
+
+        axios.post('/searchInput',{"query":query}).then((data)=>{
+            console.log(data);
+            $('.list_restaurants_wrapper').empty();
+            $('.list_restaurants_wrapper').append(searchTemplate(data));
+
+        });
+    }
+
+
+    // 맛집 클릭해서 상세 페이지로 넘어가는 이벤트
+
+
 
 }
