d1031a94c003f40b7099ec24569299c362f5300a
이성은 <seongeun47@naver.com>
2022-11-18 13:48:42 +0900

    검색 페이지(search) 상단 검색 기능 활성화

--- src/main/resources/static/resources/js/search/search.js
+++ src/main/resources/static/resources/js/search/search.js
@@ -13,12 +13,13 @@ export class Search {
     constructor() {
         console.log("Search");
         this.searchEvent();
+        this.searchDetailEvent();
     }
 
     searchEvent() {
-        console.log("검색 이벤트");
+        // console.log("검색 이벤트");
         let query = $('#query').text();
-        console.log(query)
+        console.log("검색어 : ", query);
         let searchTemplate = require('@/search/seach2-1.html');
 
         axios.post('/searchInput',{"query":query}).then((data)=>{
@@ -29,8 +30,18 @@ export class Search {
         });
     }
 
-
     // 맛집 클릭해서 상세 페이지로 넘어가는 이벤트
+    searchDetailEvent(){
+
+        // $('.list_item').on("click", (e)=>{
+        //     let resInfo = $(e.currentTarget);
+        //     console.log(resInfo);
+        //     console.log("클릭");
+        // })
+
+    }
+
+
 
 
 
