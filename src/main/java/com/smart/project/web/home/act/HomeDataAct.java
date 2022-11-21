05d32940c6f9f0858ebcb56c12289da68e0213a2
김건우 <gouonu@naver.com>
2022-11-21 16:40:32 +0900

    DB에 있는 중부원점좌표 경위도로 변환 후 찍어봤는데 이상(좌표 다름) - 데이터가 문제 있는듯 함 페이징처리 전 커밋

--- src/main/java/com/smart/project/web/home/act/HomeDataAct.java
+++ src/main/java/com/smart/project/web/home/act/HomeDataAct.java
@@ -1,18 +1,11 @@
 package com.smart.project.web.home.act;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.smart.project.component.data.CodeObject;
 import com.smart.project.proc.Test;
 import com.smart.project.web.home.vo.MainVO;
-import com.smart.project.web.home.vo.ResVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.util.*;
 
 @Slf4j
--- src/main/resources/static/resources/js/search/search.js
+++ src/main/resources/static/resources/js/search/search.js
@@ -2,7 +2,7 @@
 
 
 import listTemplate from "@/main/listCard.html";
-import searchTemplate from "@/search/seach2-1.html";
+import searchTemplate from "@/search/search2-1.html";
 
 $(()=>{
     new Search();
@@ -19,16 +19,36 @@ export class Search {
         console.log("검색 이벤트");
         let query = $('#query').text();
         console.log(query)
-        let searchTemplate = require('@/search/seach2-1.html');
+        let searchTemplate = require('@/search/search2-1.html');
 
         axios.post('/searchInput',{"query":query}).then((data)=>{
             console.log(data);
             $('.list_restaurants_wrapper').empty();
             $('.list_restaurants_wrapper').append(searchTemplate(data));
 
-        });
-    }
+            var map = new naver.maps.Map('map', {
+                center: new naver.maps.LatLng(37.3595704, 127.105399),
+                zoom: 10
+            });
 
+            // _.forEach(data.data.r, (obj)=> {
+            //     let latitude = obj.x;
+            //     let longitude = obj.y;
+            //     console.log(latitude);
+            //     console.log(longitude);
+            //     var marker = new naver.maps.Marker({
+            //         position: new naver.maps.LatLng(latitude, longitude),
+            //         map: map
+            //     });
+            // });
+
+            // pagination
+
+
+
+        });
+
+    }
 
     // 맛집 클릭해서 상세 페이지로 넘어가는 이벤트
 
