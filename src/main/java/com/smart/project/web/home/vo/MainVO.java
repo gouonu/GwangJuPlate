8f929698988ed636998fd15451d30c08c3cc977d
이성은 <seongeun47@naver.com>
2022-11-24 09:52:36 +0900

    리뷰 업데이트 날짜 수정(updateDate), 리뷰 순서 조정

--- src/main/java/com/smart/project/web/home/vo/MainVO.java
+++ src/main/java/com/smart/project/web/home/vo/MainVO.java
@@ -2,6 +2,8 @@ package com.smart.project.web.home.vo;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class MainVO {
     private String title;
@@ -9,4 +11,7 @@ public class MainVO {
     private String resNum;
     private int index;
     private String image;
+    private int listViews; // 조회수
+    private Date listDate; // 게시 날짜
+
 }
