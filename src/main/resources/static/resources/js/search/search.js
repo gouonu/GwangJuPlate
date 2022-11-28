1c794dc5535f7091c3883635fce680b0ead6ad1a
이성은 <seongeun47@naver.com>
2022-11-28 15:10:22 +0900

    검색(Search) 음식점 사진 추가

--- src/main/resources/static/resources/js/search/search.js
+++ src/main/resources/static/resources/js/search/search.js
@@ -1,9 +1,6 @@
 "use strict";
 
 
-import listTemplate from "@/main/mainListCard.html";
-import searchTemplate from "@/search/search2-1.html";
-
 $(()=>{
     new Search();
 })
@@ -22,17 +19,33 @@ export class Search {
         let searchTemplate = require('@/search/search2-1.html');
 
         axios.post('/searchInput',{"query":query}).then((data)=>{
-            console.log(data.data);
+            // console.log(data.data);
             $('.list_restaurants_wrapper').empty();
             $('.list_restaurants_wrapper').append(searchTemplate(data));
 
             data.data.r.forEach(value=>{
-                // console.log(value.num);
+                // console.log(value);
+                // console.log(value.workplace);
                 axios.post("detailCount", {"bno":value.num}).then((count)=>{
                     count = count.data;
                     // console.log($("."+value.num));
                     $("."+value.num).children().children(".restaurant_info").children("div").eq(1).children().children(".restaurant_reviews").text(count);
                 })
+
+
+                axios.post("DetailImg", {"workplace":value.workplace}).then((e)=>{
+                    // console.log(e.data);
+                    let $i =  $("." + value.num).children().children().children();
+                    if(e.data===""){
+                        // console.log(value.workplace+" 이미지 없음");
+                        $i.attr("src", "/image/empty.png");
+                    }else {
+                        console.log(value.workplace+" 이미지 있음");
+                        // console.log(e.data.img1src);
+                        $i.attr("src", e.data.img1src);
+                    }
+                })
+
             });
         });
 
