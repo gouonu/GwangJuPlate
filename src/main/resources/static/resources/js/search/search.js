6a4e5a5be5b11f30d8ac76cc1dbf7bd759c45633
김건우 <gouonu@naver.com>
2022-12-02 10:55:11 +0900

    리뷰 업데이트(이미지), search(이미지, 조회수, map)수정

--- src/main/resources/static/resources/js/search/search.js
+++ src/main/resources/static/resources/js/search/search.js
@@ -1,7 +1,8 @@
 "use strict";
 
 
-
+import listTemplate from "@/main/mainListCard.html";
+import searchTemplate from "@/search/search2-1.html";
 
 $(()=>{
     new Search();
@@ -12,8 +13,8 @@ export class Search {
     constructor() {
         console.log("Search");
         this.searchEvent();
-        this.paginationBtn();
         this.pagination();
+        this.paginationBtn();
     }
 
     searchEvent() {
@@ -37,96 +38,79 @@ export class Search {
 
         }).then((data)=>{
             console.log(data);
-            // console.log(data.data.r.length);
             $('.list_restaurants_wrapper').empty();
+            $('.list_restaurants_wrapper').append(searchTemplate(data));
 
-
-
-            if(data.data.r.length===0){ // 검색 결과 없을 때
-                $('.list_restaurants_wrapper').append("검색 결과가 없습니다.");
-            }else{ // 검색 결과 있을 때
-                $('.list_restaurants_wrapper').append(searchTemplate(data));
-
-
-                data.data.r.forEach(value=>{
-                    // console.log(value);
-                    // console.log(value.workplace);
-                    axios.post("detailCount", {"bno":value.num}).then((count)=>{
-                        count = count.data;
-                        // console.log($("."+value.num));
-                        $("."+value.num).children().children(".restaurant_info").children("div").eq(1).children().children(".restaurant_reviews").text(count);
-                    })
-
-                    axios.post("DetailImg", {"workplace":value.workplace}).then((e)=>{
-                        // console.log(e.data);
-                        let $i =  $("." + value.num).children().children().children();
-                        if(e.data===""){
-                            // console.log(value.workplace+" 이미지 없음");
-                            $i.attr("src", "/image/empty.png");
-                        }else {
-                            console.log(value.workplace+" 이미지 있음");
-                            // console.log(e.data.img1src);
-                            $i.attr("src", e.data.img1src);
-                        }
-                    })
-
-                    // map
-                    var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-                        mapOption = {
-                            center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-                            level: 8 // 지도의 확대 레벨
-                        };
-
-                    // 지도를 생성합니다
-                    var map = new kakao.maps.Map(mapContainer, mapOption);
-
-                    // 주소-좌표 변환 객체를 생성합니다
-                    var geocoder = new kakao.maps.services.Geocoder();
-
-
-                    _.forEach(data.data.r, (obj)=> {
-                        let roadAddr = obj.roadAddr;
-                        let workplace = obj.workplace;
-                        // 주소로 좌표를 검색합니다
-                        geocoder.addressSearch(roadAddr, function(result, status) {
-
-                            // 정상적으로 검색이 완료됐으면
-                            if (status === kakao.maps.services.Status.OK) {
-
-                                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-                                // 결과값으로 받은 위치를 마커로 표시합니다
-                                var marker = new kakao.maps.Marker({
-                                    map: map,
-                                    position: coords
-                                });
-
-                                var infowindow = new kakao.maps.InfoWindow({
-                                    content: '<div style="width:150px;text-align:center;padding:6px 0;">'+workplace+'</div>'
-                                });
-                                infowindow.open(map, marker);
-
-                                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-                                map.setCenter(coords);
-                            }
-                        });
-                    });
+            // 이미지, 조회수
+            data.data.r.forEach(value=> {
+                // 조회수
+                axios.post("detailCount", {"bno": value.num}).then((count) => {
+                    count = count.data;
+                    // console.log($("."+value.num));
+                    $("." + value.num).children().children(".restaurant_info").children("div").eq(1).children().children(".restaurant_reviews").text(count);
                 });
-            }
+                // 이미지
+                axios.post("DetailImg", {"workplace": value.workplace}).then((e) => {
+                    // console.log(e.data);
+                    let $i = $("." + value.num).children().children().children();
+                    if (e.data === "") {
+                        // console.log(value.workplace+" 이미지 없음");
+                        $i.attr("src", "/image/empty.png");
+                    } else {
+                        console.log(value.workplace + " 이미지 있음");
+                        // console.log(e.data.img1src);
+                        $i.attr("src", e.data.img1src);
+                    }
+                });
+            });
+
+            // map
+            var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+                mapOption = {
+                    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+                    level: 8 // 지도의 확대 레벨
+                };
+
+            // 지도를 생성합니다
+            var map = new kakao.maps.Map(mapContainer, mapOption);
+
+            // 주소-좌표 변환 객체를 생성합니다
+            var geocoder = new kakao.maps.services.Geocoder();
 
 
+            _.forEach(data.data.r, (obj)=> {
+                let roadAddr = obj.roadAddr;
+                let workplace = obj.workplace;
+                // 주소로 좌표를 검색합니다
+                geocoder.addressSearch(roadAddr, function(result, status) {
 
+                    // 정상적으로 검색이 완료됐으면
+                    if (status === kakao.maps.services.Status.OK) {
 
+                        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                        // 결과값으로 받은 위치를 마커로 표시합니다
+                        var marker = new kakao.maps.Marker({
+                            map: map,
+                            position: coords
+                        });
+
+                        var infowindow = new kakao.maps.InfoWindow({
+                            content: '<div style="width:150px;text-align:center;padding:6px 0;">'+workplace+'</div>'
+                        });
+                        infowindow.open(map, marker);
+
+                        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                        map.setCenter(coords);
+                    }
+                });
+            });
 
 
         }); // then
 
 
     }//serchEvent
-
-    imgAdd(){
-
-    }
 
 
 
@@ -162,17 +146,15 @@ export class Search {
                 // 지도 다시 넣기
                 $('.list_map').append('<div id="map" style="width:400px;height:500px;"></div>');
 
-
-
+                // 페이징 넘길 때 이미지, 조회수
                 data.data.r.forEach(value=> {
-                    // console.log(value);
-                    // console.log(value.workplace);
+                    // 조회수
                     axios.post("detailCount", {"bno": value.num}).then((count) => {
                         count = count.data;
                         // console.log($("."+value.num));
                         $("." + value.num).children().children(".restaurant_info").children("div").eq(1).children().children(".restaurant_reviews").text(count);
                     });
-
+                    // 이미지
                     axios.post("DetailImg", {"workplace": value.workplace}).then((e) => {
                         // console.log(e.data);
                         let $i = $("." + value.num).children().children().children();
@@ -186,8 +168,6 @@ export class Search {
                         }
                     });
                 });
-
-
 
                 // map
                 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
@@ -205,6 +185,7 @@ export class Search {
 
 
                 _.forEach(data.data.r, (obj)=> {
+
                     let roadAddr = obj.roadAddr;
                     let workplace = obj.workplace;
                     // 주소로 좌표를 검색합니다
@@ -232,6 +213,8 @@ export class Search {
                     });
                 });
 
+
+
             });
         });
 
@@ -249,16 +232,8 @@ export class Search {
             } else {
                 $(e.currentTarget).addClass('active');
             }
-
-
         });
     }
 
 
 }// Search
-
-
-
-
-
-
